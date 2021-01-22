9582455f788d7bbbe08eff7153243b29985ac1a3
HemanthJabalpuri <hemanthjabalpuri@gmail.com>
2021-01-22 16:49:17 +0530

    Remove mtk-su specific script

--- magisk-boot.sh
+++ magisk-boot.sh
@@ -1,22 +1,38 @@
 #!/system/bin/sh
 #
-# Updated: Mar 31, 2020
-# by diplomatic
+# This script is based on magisk-boot.sh from
+# https://forum.xda-developers.com/t/amazing-temp-root-for-mediatek-armv8-2020-08-24.3922213/
+# by diplomatic @ XDA dated Mar 31, 2020
 #
-# This script sets up bootless root with Magisk on MediaTek Android devices.
-# It uses mtk-su, the temporary root tool for MediaTek ARMv8 chips. Must be run
-# from the app 'init.d scripts support' by RYO Software. Put this file into
-# /storage/emulated/0/init.d along with mtk-su and magiskinit into .../init.d/bin
+# This script sets up bootless root with Magisk on Android devices.
+# Must be run as root for executing this script and we have to do this on every boot.
+# We can run this from the app 'init.d scripts support' by RYO Software which can run scripts on boot.
+# Put this file into /storage/emulated/0/init.d and put magiskinit into .../init.d/bin
 # Point the app to run sh scripts from /storage/emulated/0/init.d at boot time.
 #
+# You can use whatever you want to execute on every boot.
+#
 # WARNING: DO NOT UPDATE MAGISK THROUGH MAGISK MANAGER OR YOU WILL BRICK YOUR
-#		   DEVICE ON A LOCKED BOOTLOADER
+#          DEVICE ON A LOCKED BOOTLOADER
 #
 
-HOMEDIR=/data/data/com.ryosoftware.initd/files/bin
+HOMEDIR=/data/local/tmp
 SRCDIR=/storage/emulated/0/init.d
 
-SU_MINISCRIPT='
+mkdir -p $HOMEDIR
+cd $HOMEDIR || exit 1
+
+if ! cmp -s $SRCDIR/bin/magiskinit magiskinit; then
+  cp $SRCDIR/bin/magiskinit ./
+  chmod 700 magiskinit
+
+  ln -fs magiskinit magiskpolicy
+  ln -fs magiskinit magisk
+fi
+
+# start SU daemon
+export HOMEDIR
+
 # Magisk function to find boot partition and prevent the installer from finding
 # it again
 find_block() {
@@ -46,8 +62,8 @@ find_block() {
 
 # Root only at this point; hoping selinux is permissive
 if [ $(id -u) != 0 ] || [ "$(getenforce)" != "Permissive" ]; then
-	echo "Root user only" >&2
-	exit 1
+  echo "Root user only" >&2
+  exit 1
 fi
 
 # Disaster prevention
@@ -59,81 +75,57 @@ cd $HOMEDIR || { setenforce 1; exit 1; }
 # Patch selinux policy
 ./magiskpolicy --live --magisk "allow magisk * * *"
 if [ ! -f /sbin/.init-stamp ]; then
-	# Set up /root links to /sbin files
-	mount | grep -qF rootfs
-	have_rootfs=$?
-	if [ $have_rootfs -eq 0 ]; then
-		mount -o rw,remount /
-		mkdir -p /root
-		chmod 750 /root
-		if ! ln /sbin/* /root; then
-			echo "Error making /sbin hardlinks" >&2
-			setenforce 1
-			exit 1
-		fi
-		mount -o ro,remount /
-	fi
-	# Create tmpfs /sbin overlay
-	# This may crash on system-as-root with no /root directory
-	./magisk -c >&2
+  # Set up /root links to /sbin files
+  mount | grep -qF rootfs
+  have_rootfs=$?
+  if [ $have_rootfs -eq 0 ]; then
+    mount -o rw,remount /
+    mkdir /root
+    chmod 750 /root
+    if ! ln /sbin/* /root; then
+      echo "Error making /sbin hardlinks" >&2
+      mount -o ro,remount /
+      setenforce 1
+      exit 1
+    fi
+    mount -o ro,remount /
+  fi
+  # Create tmpfs /sbin overlay
+  # This may crash on system-as-root with no /root directory
+  ./magisk -c >&2
 
-	touch /sbin/.init-stamp
+  touch /sbin/.init-stamp
 
-	if [ ! -f /sbin/magiskinit ] || [ ! -f /sbin/magisk ]; then
-		echo "Bad /sbin mount?" >&2
-		setenforce 1
-		exit 1
-	fi
+  if [ ! -f /sbin/magiskinit ] || [ ! -f /sbin/magisk ]; then
+    echo "Bad /sbin mount?" >&2
+    setenforce 1
+    exit 1
+  fi
 
-	# Copy binaries
-	cp magiskinit /sbin/
+  # Copy binaries
+  cp magiskinit /sbin/
 
-	if [ $have_rootfs -ne 0 ]; then
-		mkdir /sbin/.magisk/mirror/system_root
-		block=$(mount | grep " / " | cut -d\  -f1)
-		[ $block = "/dev/root" ] && block=/dev/block/dm-0
-		mount -o ro $block /sbin/.magisk/mirror/system_root
-		for file in /sbin/.magisk/mirror/system_root/sbin/*; do
-			if [ -L $file ]; then
-			  cp -a $file /sbin/
-			else
-			  cp -ps $file /sbin/${file##*/}
-			fi
-		done
-	fi
+  if [ $have_rootfs -ne 0 ]; then
+    mkdir /sbin/.magisk/mirror/system_root
+    block=$(mount | grep " / " | cut -d\  -f1)
+    [ $block = "/dev/root" ] && block=/dev/block/dm-0
+    mount -o ro $block /sbin/.magisk/mirror/system_root
+    for file in /sbin/.magisk/mirror/system_root/sbin/*; do
+      if [ -L $file ]; then
+        cp -a $file /sbin/
+      else
+        cp -ps $file /sbin/${file##*/}
+      fi
+    done
+  fi
 
-	export PATH=/sbin:$PATH
+  export PATH=/sbin:$PATH
 
-	# Finish startup calls
-	magisk --post-fs-data
-	sleep 1		# hack to prevent race with later service calls
-	magisk --service
-	magisk --boot-complete
+  # Finish startup calls
+  magisk --post-fs-data
+  sleep 1        # hack to prevent race with later service calls
+  magisk --service
+  magisk --boot-complete
 fi
 
 setenforce 1
-'
-
-mkdir -p $HOMEDIR
-cd $HOMEDIR || exit 1
-
-if ! cmp -s $SRCDIR/bin/magiskinit magiskinit; then
-	cp $SRCDIR/bin/magiskinit ./
-	chmod 700 magiskinit
-
-	ln -fs magiskinit magiskpolicy
-	ln -fs magiskinit magisk
-fi
-
-if ! cmp -s $SRCDIR/bin/mtk-su mtk-su; then
-	cp $SRCDIR/bin/mtk-su ./
-	chmod 700 mtk-su
-fi
-
-# strip ':c512...' tail from selinux context
-ctx=$(cat /proc/$$/attr/current)
-newctx=${ctx/%:s0:*/:s0}
-
-# start SU daemon
-export HOMEDIR
-echo "$SU_MINISCRIPT" | ./mtk-su -Z $newctx
