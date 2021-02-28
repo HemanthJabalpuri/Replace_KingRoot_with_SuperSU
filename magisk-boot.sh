99e2bd8b0306ca157038560e022a41dbb36f4ca1
HemanthJabalpuri <hemanthjabalpuri@gmail.com>
2021-02-28 16:45:24 +0530

    Add Magisk v22+ support and remove older versions support

--- magisk-boot.sh
+++ magisk-boot.sh
@@ -4,10 +4,11 @@
 # https://forum.xda-developers.com/t/amazing-temp-root-for-mediatek-armv8-2020-08-24.3922213/
 # by diplomatic @ XDA dated Mar 31, 2020
 #
-# This script sets up bootless root with Magisk on Android devices.
+# This script sets up bootless root with Magisk v22+ on Android devices.
 # Must be run as root for executing this script and we have to do this on every boot.
 # We can run this from the app 'init.d scripts support' by RYO Software which can run scripts on boot.
-# Put this file into /storage/emulated/0/init.d and put magiskinit into .../init.d/bin
+# Put this file into /storage/emulated/0/init.d and put magisk.apk into .../init.d/bin
+#
 # Point the app to run sh scripts from /storage/emulated/0/init.d at boot time.
 #
 # You can use whatever you want to execute on every boot.
@@ -17,27 +18,27 @@
 #
 
 HOMEDIR=/data/local/tmp
-SRCDIR=/storage/emulated/0/init.d
+SRCDIR=/sdcard/init.d
+
+API=`getprop ro.build.version.sdk`
+ABI=`getprop ro.product.cpu.abi | dd bs=1 count=3 2>/dev/null`
+ABI2=`getprop ro.product.cpu.abi2 | dd bs=1 count=3 2>/dev/null`
+ABILONG=`getprop ro.product.cpu.abi`
+
+ARCH=arm
+IS64BIT=false
+[ "$ABI" = "x86" ] && ARCH=x86
+[ "$ABI2" = "x86" ] && ARCH=x86
+if [ "$ABILONG" = "arm64-v8a" ]; then ARCH=arm; IS64BIT=true; fi
+if [ "$ABILONG" = "x86_64" ]; then ARCH=x86; IS64BIT=true; fi
 
 if [ "$1" = "info" ]; then
-  API=`getprop ro.build.version.sdk`
-  ABI=`getprop ro.product.cpu.abi | dd bs=1 count=3 2>/dev/null`
-  ABI2=`getprop ro.product.cpu.abi2 | dd bs=1 count=3 2>/dev/null`
-  ABILONG=`getprop ro.product.cpu.abi`
-
-  ARCH=arm
-  IS64BIT=false
-  [ "$ABI" = "x86" ] && ARCH=x86
-  [ "$ABI2" = "x86" ] && ARCH=x86
-  if [ "$ABILONG" = "arm64-v8a" ]; then ARCH=arm; IS64BIT=true; fi
-  if [ "$ABILONG" = "x86_64" ]; then ARCH=x86; IS64BIT=true; fi
-
-  [ "$API" -lt 17 ] && echo " Magisk is only for 4.2(17)+" && exit
   echo "API is $API"
   echo "Architecture is $ARCH"
   echo "64bit == $IS64BIT"
   exit
 fi
+[ "$API" -lt 17 ] && echo " Magisk is only for 4.2(17)+" && exit
 
 # SELinux stuffs
 SELINUX=false
@@ -52,33 +53,47 @@ if [ "$notsu" = 1 ]; then
 elif $SELINUX && [ "$(getenforce)" != "Permissive" ]; then
   echo "SELinux not Permissive" >&2
   exit 1
+elif [ -f /sbin/.init-stamp ]; then
+  exit 1
 fi
 
-INITF=$SRCDIR/bin/magiskinit
+MAPK=$SRCDIR/bin/magisk.apk
 if [ -n "$1" ]; then
   tmpd="$PWD"; [ "$PWD" = "/" ] && tmpd=""
   case "$1" in
     /*) fpath="$1";;
     *) fpath="$tmpd/${1#./}";;
   esac
-  INITF="$fpath"
+  MAPK="$fpath"
 fi
-if ! [ -f $INITF ] && ! [ -f $HOMEDIR/magiskinit ]; then
-  echo "magiskinit not found" >&2
+if ! [ -f "$MAPK" ]; then
+  echo "magisk apk not found" >&2
   exit
 fi
 
-mkdir -p $HOMEDIR
-cd $HOMEDIR || exit 1
+MBIN=$HOMEDIR/magiskbin
+mkdir -p $MBIN
+cd $MBIN || exit 1
 
-if ! cmp $INITF magiskinit >/dev/null 2>&1; then
-  cp -f $INITF ./magiskinit
-  chmod 700 magiskinit
-
-  rm magiskpolicy magisk >/dev/null 2>&1
-  $SELINUX && ln -s magiskinit magiskpolicy
-  ln -s magiskinit magisk
+BUSYBOX="$(command -v busybox)"
+if [ -z "$BUSYBOX" ]; then
+  echo "Install busybox" >&2
+  exit
 fi
+#BUSYBOX=/data/busybox && chmod 777 $BUSYBOX
+[ "$ARCH" = "arm" ] && ARCH=armeabi-v7a
+MTMP=$HOMEDIR/magisktmp
+mkdir $MTMP
+$BUSYBOX unzip -q "$MAPK" -d $MTMP || exit 1
+cd $MTMP/lib/$ARCH
+for libfile in lib*.so; do
+  file="${libfile#lib}"; file="${file%.so}"
+  mv "$libfile" "$MBIN/$file"
+  chmod 700 "$MBIN/$file"
+done
+cd -
+mv $MTMP/assets/* $MBIN
+rm -rf $MTMP
 
 find_fun() {
   if [ -n "$(command -v find)" ]; then
@@ -131,67 +146,81 @@ find_boot() {
 # Disaster prevention
 find_boot $(getprop ro.boot.slot_suffix)
 
-cd $HOMEDIR || { $SELINUX && setenforce 1; exit 1; }
+cd $HOMEDIR
+rm magiskpolicy magisk >/dev/null 2>&1
+$SELINUX && ln -s $MBIN/magiskinit magiskpolicy
+ln -s $MBIN/magiskinit magisk
 
 # Patch selinux policy
 $SELINUX && ./magiskpolicy --live --magisk "allow magisk * * *"
-if [ ! -f /sbin/.init-stamp ]; then
-  # Set up /root links to /sbin files
-  mount | grep -qF rootfs
-  have_rootfs=$?
-  if [ $have_rootfs -eq 0 ]; then
-    echo "Have rootfs"
-    mount -o rw,remount /
-    mkdir /root 2>/dev/null
-    chmod 750 /root
-    for i in /sbin/*; do
-      ln $i /root/${i/'/sbin/'/}
-      if [ $? != 0 ]; then
-        echo "Error making /sbin hardlinks" >&2
-        mount -o ro,remount /
-        $SELINUX && setenforce 1
-        exit 1
-      fi
-    done
-    mount -o ro,remount /
-  fi
-  # Create tmpfs /sbin overlay
-  # This may crash on system-as-root with no /root directory
-  ./magisk -c >&2
 
-  touch /sbin/.init-stamp
-
-  if [ ! -f /sbin/magiskinit ] || [ ! -f /sbin/magisk ]; then
-    echo "Bad /sbin mount?" >&2
-    $SELINUX && setenforce 1
-    exit 1
-  fi
-
-  # Copy binaries
-  cp magiskinit /sbin/
-
-  if [ $have_rootfs -ne 0 ]; then
-    mkdir /sbin/.magisk/mirror/system_root
-    block=$(mount | grep " / " | cut -d\  -f1)
-    [ $block = "/dev/root" ] && block=/dev/block/dm-0
-    mount -o ro $block /sbin/.magisk/mirror/system_root
-    for file in /sbin/.magisk/mirror/system_root/sbin/*; do
-      if [ -L $file ]; then
-        cp -a $file /sbin/
-      else
-        cp -ps $file /sbin/${file##*/}
-      fi
-    done
-  fi
-
-  export PATH=/sbin:$PATH
-
-  # Finish startup calls
-  magisk --post-fs-data
-  sleep 1        # hack to prevent race with later service calls
-  magisk --service
-  magisk --boot-complete
+# Set up /root links to /sbin files
+mount | grep -qF rootfs
+have_rootfs=$?
+if [ $have_rootfs -eq 0 ]; then
+  echo "Have rootfs"
+  mount -o rw,remount /
+#  rm -rf /root
+  mkdir /root 2>/dev/null
+  chmod 750 /root
+  for i in /sbin/*; do
+    ln $i /root/${i/'/sbin/'/}
+    if [ $? != 0 ]; then
+      echo "Error making /sbin hardlinks" >&2
+      mount -o ro,remount /
+      $SELINUX && setenforce 1
+      exit 1
+    fi
+  done
+  # copy xz compressed magisk binaries to /sbin
+  $MBIN/magiskboot compress=xz $MBIN/magisk32 /sbin/magisk32.xz
+  $IS64BIT && $MBIN/magiskboot compress=xz $MBIN/magisk64 /sbin/magisk64.xz
+  mount -o ro,remount /
 fi
+
+# Create tmpfs /sbin overlay
+# This may crash on system-as-root with no /root directory
+./magisk -c >&2
+
+touch /sbin/.init-stamp
+
+if [ ! -f /sbin/magiskinit ] || [ ! -f /sbin/magisk ]; then
+  echo "Bad /sbin mount?" >&2
+  $SELINUX && setenforce 1
+  exit 1
+fi
+
+# Copy binaries
+cp $MBIN/magiskinit /sbin/
+if ! [ -f /data/adb/magisk/util_functions.sh ]; then
+  INSTALL_PATH=/data/user_de/0/com.topjohnwu.magisk/install/
+  [ $API -lt 24 ] && INSTALL_PATH=/data/data/com.topjohnwu.magisk/install/
+  mkdir $INSTALL_PATH 2>/dev/null
+  mv $MBIN/* $INSTALL_PATH
+fi
+rm -rf magisk*
+
+if [ $have_rootfs -ne 0 ]; then
+  mkdir /sbin/.magisk/mirror/system_root
+  block=$(mount | grep " / " | cut -d\  -f1)
+  [ $block = "/dev/root" ] && block=/dev/block/dm-0
+  mount -o ro $block /sbin/.magisk/mirror/system_root
+  for file in /sbin/.magisk/mirror/system_root/sbin/*; do
+    if [ -L $file ]; then
+      cp -a $file /sbin/
+    else
+      cp -ps $file /sbin/${file##*/}
+    fi
+  done
+fi
+
+export PATH=/sbin:$PATH
+
+# Finish startup calls
+magisk --post-fs-data
+sleep 1        # hack to prevent race with later service calls
+magisk --service
+magisk --boot-complete
 
 $SELINUX && setenforce 1
 echo "Done..."
